e09b485aeb0cbbe3c6c0acda1053f992c6c042d3
Felipe Vaccarini <felipe.jvp@hotmail.com>
2023-09-23 20:35:00 -0300

    Tabela pronta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TableComponent } from './table/table.component';
 
+import { MatTableModule } from '@angular/material/table'; // Importe o módulo da tabela
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +20,12 @@ import { TableComponent } from './table/table.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-table',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent {
+  displayedColumns: string[] = ['id', 'nome', 'descricao', 'preco']; // Declare a propriedade displayedColumns
+
+  dataSource = new MatTableDataSource<any>([]);
+
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor() {
+    this.dataSource = new MatTableDataSource([
+      { id: 1, nome: 'Item 1', descricao: 'Descrição do Item 1', preco: 10.99 },
+      { id: 2, nome: 'Item 2', descricao: 'Descrição do Item 2', preco: 11.99 },
+      { id: 3, nome: 'Item 3', descricao: 'Descrição do Item 3', preco: 12.99 },
+      // ...
+    ]);
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  aplicarFiltroGlobal(event: any) {
+    const filtro = event.target.value.trim().toLowerCase();
+    this.dataSource.filter = filtro;
+  }
+  
 
 }
